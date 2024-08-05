8d650bc6ab13da8f5bd1f5d5862f380d323e589d
Michal <michalochecki00@gmail.com>
2024-08-05 16:21:58 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import './App.css'
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import PokemonDetailsPage from './pages/PokemonDetailsPage';
-import MyRosterPage from './pages/MyRosterPage';
-import BattlePage from './pages/BattlePage';
-import LeaderboardPage from './pages/LeaderboardPage';
+// import Navbar from './components/Navbar';
+import Home from './pages/HomePage';
+import PokemonDetails from './pages/PokemonDetailsPage';
+import MyRoster from './pages/MyRosterPage';
+import Battle from './pages/BattlePage';
+import Leaderboard from './pages/LeaderboardPage';
 import Layout from './components/Layout';
 
 function App() { 
