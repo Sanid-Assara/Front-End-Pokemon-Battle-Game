e3c4e06a50fa4f1bc130c3775451eb25d40588b4
Mohamed Moghazi <eng.moghazy@gmail.com>
2024-08-08 19:45:37 +0200

    cleanup

--- src/components/PokemonSelection.jsx
+++ src/components/PokemonSelection.jsx
@@ -5,7 +5,10 @@ const PokemonSelection = ({ pokemonList, selectedPokemon, setSelectedPokemon }) 
   return (
     <div className="mb-4">
       <label className="block text-sm font-medium text-gray-700 mb-2">Select your Pokémon:</label>
-      <div className="grid grid-cols-2 gap-4">
+      <div 
+        className="grid grid-cols-6 gap-4 overflow-y-auto"
+        style={{ gridAutoRows: 'minmax(0, 1fr)' }}
+      >
         {pokemonList.map((pokemon, index) => (
           <PokemonListItem 
             key={index} 
